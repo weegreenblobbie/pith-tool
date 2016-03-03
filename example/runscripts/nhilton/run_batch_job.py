34da57f0a40477d1a6e4e89b080a425e576d968f
Nick Hilton <weegreenblobbie@yahoo.com>
2016-03-02 18:36:22 -0800

    fixes for issue #6

--- example/runscripts/nhilton/run_batch_job.py
+++ example/runscripts/nhilton/run_batch_job.py
@@ -1,3 +1,5 @@
+import argparse
+
 from module_a.fun_1 import fun_1
 from module_c.fun_4 import fun_4
 
@@ -6,7 +8,21 @@ from external_a.extra_fun import extra_fun
 
 def main():
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'input_txt_file',
+        help = 'Some input file',
+    )
+
+    args = parser.parse_args()
+
     print('Running batch job ...')
+
+    with open(args.input_txt_file, 'r') as fd:
+        text = fd.read()
+
+    print('Read "%s" from file' % repr(text))
 
     fun_1()
     fun_4()
